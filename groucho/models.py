a61efb1a0bb01171963716552d4dddfa8c23e5e7
nullkeys <akeys6803@gmail.com>
2018-01-20 10:33:55 -0500

    Adds basic requirements for groucho and django admin
    This adds admin models along with string overloads on database models
    for the django admin site.

--- groucho/models.py
+++ groucho/models.py
@@ -5,12 +5,23 @@ class Configuration(models.Model):
     invalid_password_message = models.CharField(max_length=100)
     new_user_exists_rate = models.IntegerField(blank=False, default=20)
 
+    def __str__(self):
+        return 'Application Configuration Options'
+
+
 class AttemptUser(models.Model):
     username = models.CharField(max_length=100, blank=False)
     exists = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
     
 class AttemptSource(models.Model):
     credentials = models.ForeignKey(AttemptUser, on_delete=models.CASCADE)
     ip = models.GenericIPAddressField(protocol='IPv4', blank=False)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.ip
